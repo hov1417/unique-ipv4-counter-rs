edc35d60afac663f64b24dccdf7a896777c7bf00
hov1417 <hovhannes1417@gmail.com>
2024-11-15 22:41:46 +0400

    feat: work in progress

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,63 @@
+#![allow(internal_features)]
 #![feature(core_intrinsics)]
-extern crate core;
 
 use std::fs::File;
 use std::io;
 use std::mem::transmute;
 use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
-use std::sync::Arc;
 use std::thread;
-
-const CHUNK_SIZE: usize = 1 * 1024 * 1024; // 8 MB
+use thread_priority::ThreadPriority;
 
 /// A set to keep track of unique IPs using a bitmask.
-#[derive(Default)]
 struct MaxBitSet {
+    // words: Box<[AtomicU64; 1 << 26]>,
     words: Vec<AtomicU64>,
 }
 
 impl MaxBitSet {
     fn new() -> Self {
-        // Pre-allocate memory to cover all IP addresses
+        // let words = Box::new([const { AtomicU64::new(0) }; 1 << 26]);
         let words = vec![0; 1 << 26].into_iter().map(AtomicU64::new).collect();
         Self { words }
     }
 
-    #[inline(never)]
+    // // #[inline(never)]
     /// Sets the bit at the specified index.
     fn set(&self, bit_index: u32) {
-        let word_index = bit_index >> 6;
-        let mask = 1 << (bit_index % 64);
-
-        let words = self.words.get(word_index as usize).unwrap();
-        words.fetch_or(mask, Ordering::SeqCst);
+        // self.words[bit_index.wrapping_shr(6) as usize].fetch_or(mask, Ordering::SeqCst);
+        let word = self.get_atomic(bit_index);
+        self.set_bit(word, bit_index);
     }
 
-    #[inline(never)]
+    // #[inline(never)]
+    fn set_bit(&self, word: &AtomicU64, bit_index: u32) {
+        let mask = 1u64.wrapping_shl(bit_index);
+        loop {
+            let cur = word.load(Ordering::Relaxed);
+            let new = cur | mask;
+            if new == cur
+                || word
+                .compare_exchange_weak(cur, new, Ordering::Relaxed, Ordering::Relaxed)
+                .is_ok()
+            {
+                return;
+            }
+        }
+        // let mask = 1u64.wrapping_shl(bit_index);
+        // AtomicU64::fetch_or(word, mask, Ordering::SeqCst)
+    }
+
+    // #[inline(never)]
+    fn get_atomic(&self, bit_index: u32) -> &AtomicU64 {
+        unsafe {
+            &*self
+                .words
+                .as_ptr()
+                .offset(bit_index.wrapping_shr(6) as isize)
+        }
+    }
+
+    // #[inline(never)]
     /// Returns the number of unique bits set.
     fn count(&self) -> u64 {
         self.words
@@ -43,7 +67,7 @@ impl MaxBitSet {
     }
 }
 
-#[inline(never)]
+// #[inline(never)]
 fn num(mut value: u128, new_line: u32) -> u32 {
     value = value & 0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F;
     let mut shift_size = 24;
@@ -66,9 +90,11 @@ fn num(mut value: u128, new_line: u32) -> u32 {
     result | (current_byte as u32)
 }
 
-#[inline(never)]
+// #[inline(never)]
 fn read_wide(bytes: &[u8]) -> u128 {
-    if std::intrinsics::unlikely(bytes.len() < 16) {
+    if std::intrinsics::likely(bytes.len() >= 16) {
+        u128::from_le_bytes(bytes[..16].try_into().unwrap())
+    } else {
         let mut wide = 0u128;
         let mut offset = 0;
         for &b in bytes.iter().take(16) {
@@ -76,12 +102,10 @@ fn read_wide(bytes: &[u8]) -> u128 {
             offset += 8;
         }
         wide
-    } else {
-        u128::from_le_bytes(bytes[..16].try_into().unwrap())
     }
 }
 
-#[inline(never)]
+// #[inline(never)]
 fn newline_location(word: u128) -> u32 {
     let reversed = word ^ 0x0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A0A; // xor with \n\n\n...\n\n\n
     let code = (reversed.wrapping_sub(0x01010101010101010101010101010101))
@@ -90,13 +114,14 @@ fn newline_location(word: u128) -> u32 {
     code.trailing_zeros() >> 3
 }
 
-#[inline(never)]
+// #[inline(never)]
 fn worker(
     memory: &'static [u8],
-    chunk_id: Arc<AtomicUsize>,
-    bitset: Arc<MaxBitSet>,
+    chunk_id: &'static AtomicUsize,
+    bitset: &'static MaxBitSet,
     file_size: usize,
 ) {
+    const CHUNK_SIZE: usize = 1 * 1024 * 1024;
     loop {
         let id = chunk_id.fetch_add(1, Ordering::SeqCst);
         if std::intrinsics::unlikely(id * CHUNK_SIZE >= memory.len()) {
@@ -111,12 +136,17 @@ fn worker(
             current_mem = &current_mem[newline_location(value) as usize..];
         }
 
-        while current_mem.as_ptr() < end {
-            let value = read_wide(current_mem);
-            let next_new = newline_location(value) + 1;
-            bitset.set(num(value, next_new));
-            current_mem = &current_mem[(next_new as usize)..];
-        }
+        parse_loop(bitset, current_mem, end);
+    }
+}
+
+// #[inline(never)]
+fn parse_loop(bitset: &MaxBitSet, mut current_mem: &[u8], end: *const u8) {
+    while current_mem.as_ptr() < end {
+        let value = read_wide(current_mem);
+        let next_new = newline_location(value) + 1;
+        bitset.set(num(value, next_new));
+        current_mem = &current_mem[(next_new as usize)..];
     }
 }
 
@@ -129,37 +159,51 @@ fn main() -> io::Result<()> {
     let file_path = &args[1];
 
     let file = File::open(file_path)?;
-    let file_size = file.metadata()?.len() as usize;
+
+    let count = count_ips(&file);
+
+    println!("Number of unique IP addresses: {}", count);
+    Ok(())
+}
+
+fn count_ips(file: &File) -> u64 {
+    let file_size = file.metadata().unwrap().len() as usize;
 
     // Create memory-mapped file
-    let mmap = unsafe { memmap::Mmap::map(&file).unwrap() };
+    let mmap = Box::leak(Box::new(unsafe { memmap2::Mmap::map(file).unwrap() }));
     let map: &'static [u8] = unsafe { transmute::<&[u8], &'static [u8]>(mmap.as_ref()) };
 
-    let chunk_id = Arc::new(AtomicUsize::new(0));
-    let bitset = Arc::new(MaxBitSet::new());
+    let chunk_id = Box::leak(Box::new(AtomicUsize::new(0)));
+    let bitset = Box::leak(Box::new(MaxBitSet::new()));
 
     let mut handles = Vec::new();
     let num_threads = num_cpus::get();
     for _ in 0..num_threads {
-        let chunk_id_clone = Arc::clone(&chunk_id);
-        let bitset_clone = Arc::clone(&bitset);
-
-        handles.push(thread::spawn(move || {
-            worker(map, chunk_id_clone, bitset_clone, file_size);
-        }));
+        handles.push(thread::spawn(thread(map, chunk_id, bitset, file_size)));
     }
 
     for handle in handles {
         handle.join().unwrap();
     }
+    bitset.count()
+}
 
-    println!("Number of unique IP addresses: {}", bitset.count());
-    Ok(())
+fn thread(
+    map: &'static [u8],
+    chunk_id: &'static AtomicUsize,
+    bitset: &'static MaxBitSet,
+    file_size: usize,
+) -> impl FnOnce() {
+    move || {
+        thread_priority::set_current_thread_priority(ThreadPriority::Max).unwrap();
+        worker(map, chunk_id, bitset, file_size);
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{newline_location, num, read_wide};
+    use rand::Rng;
 
     // #[test]
     // fn generate_test_file() {
@@ -167,12 +211,12 @@ mod tests {
     //     use std::fs::File;
     //     use std::io::{self, Write};
     //     use std::io::BufWriter;
-    // 
+    //
     //     /// Generates `n` random IP addresses and writes them to the specified file.
     //     fn generate_ips_and_write_to_file(filename: &str, n: usize) -> io::Result<()> {
     //         let mut file = BufWriter::new(File::create(filename)?);
     //         let mut rng = rand::thread_rng();
-    // 
+    //
     //         for _ in 0..n {
     //             // Generate a random IP address by creating four random octets.
     //             let ip = format!(
@@ -182,16 +226,16 @@ mod tests {
     //                 rng.gen::<u8>(),
     //                 rng.gen::<u8>()
     //             );
-    // 
+    //
     //             writeln!(file, "{}", ip)?;
     //         }
-    // 
+    //
     //         Ok(())
     //     }
-    // 
-    //     let filename = "target/ips-1000000000.txt";
-    //     let n = 1_000_000_000;
-    // 
+    //
+    //     let filename = "target/ips-100.txt";
+    //     let n = 100;
+    //
     //     generate_ips_and_write_to_file(filename, n).unwrap();
     //     println!("Generated {} IP addresses and wrote to {}", n, filename);
     // }
@@ -200,7 +244,10 @@ mod tests {
     fn ip_to_index(ip: &[u8]) -> Option<u32> {
         let parts: Vec<&[u8]> = ip.split(|&c| c == '.' as u8).collect();
         if parts.len() == 4 {
-            let octets: Vec<u8> = parts.iter().filter_map(|s| String::from_utf8_lossy(s).parse().ok()).collect();
+            let octets: Vec<u8> = parts
+                .iter()
+                .filter_map(|s| String::from_utf8_lossy(s).parse().ok())
+                .collect();
             if octets.len() == 4 {
                 Some(
                     ((octets[0] as u32) << 24)
@@ -216,7 +263,25 @@ mod tests {
         }
     }
 
-
+    /// Converts IP string to an integer bit index.
+    fn ip_to_index_str(ip: &str) -> Option<u32> {
+        let parts: Vec<&str> = ip.split('.').collect();
+        if parts.len() == 4 {
+            let octets: Vec<u8> = parts.iter().filter_map(|s| s.parse().ok()).collect();
+            if octets.len() == 4 {
+                Some(
+                    ((octets[0] as u32) << 24)
+                        | ((octets[1] as u32) << 16)
+                        | ((octets[2] as u32) << 8)
+                        | (octets[3] as u32),
+                )
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
 
     #[test]
     fn location_test_1() {
@@ -289,5 +354,34 @@ mod tests {
         let location = newline_location(u128::from_le_bytes(data));
         let number = num(read_wide(&data), location);
         assert_eq!(number, 0x01020304u32);
+    }
+
+    #[test]
+    fn parse_fuzzy_test() {
+        let mut rng = rand::thread_rng();
+        let mut ip_str = move || {
+            format!(
+                "{}.{}.{}.{}",
+                rng.gen::<u8>(),
+                rng.gen::<u8>(),
+                rng.gen::<u8>(),
+                rng.gen::<u8>()
+            )
+        };
+        for _ in 0..100000 {
+            let ip1 = ip_str();
+            let ip2 = ip_str();
+            let data = format!("{ip1}\n{ip2}\n");
+            let data_bytes = data.as_bytes()[0..16].try_into().unwrap();
+            let location = newline_location(u128::from_le_bytes(data_bytes));
+            assert_eq!(
+                ip1.len(),
+                location as usize,
+                "length mismatch {ip1:?} {location}"
+            );
+            let number = num(read_wide(&data_bytes), location);
+            let number_real = ip_to_index_str(&ip1).unwrap();
+            assert_eq!(number, number_real, "parsing this chunk failed '{data:?}'");
+        }
     }
 }
