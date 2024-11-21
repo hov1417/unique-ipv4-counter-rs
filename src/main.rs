79a972b1d9f2e716f1ca0f8b8de7f1f158fe3d0e
hov1417 <hovhannes1417@gmail.com>
2024-11-21 21:51:11 +0400

    feat: cleanup, thread priority feature, removing unsafe where possible

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![feature(core_intrinsics)]
 #![feature(portable_simd)]
 #![feature(integer_sign_cast)]
+#![feature(file_lock)]
 
 mod shuffle_pattern;
 
@@ -12,7 +13,7 @@ use std::simd::cmp::SimdPartialEq;
 use std::simd::{Mask, Simd};
 use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
 use std::thread;
-use std::{io, simd};
+use std::io;
 
 /// A set to keep track of unique IPs using a bitmask.
 struct MaxBitSet {
@@ -80,12 +81,7 @@ fn newline_location(word: Simd<u8, 16>) -> u32 {
         .trailing_zeros()
 }
 
-fn worker(
-    memory: &'static [u8],
-    chunk_id: &'static AtomicUsize,
-    bitset: &'static MaxBitSet,
-    file_size: usize,
-) {
+fn worker(memory: &'static [u8], chunk_id: &'static AtomicUsize, bitset: &'static MaxBitSet) {
     const CHUNK_SIZE: usize = 1024 * 1024;
     loop {
         let id = chunk_id.fetch_add(1, Ordering::SeqCst);
@@ -93,7 +89,7 @@ fn worker(
             break;
         }
         let mut current_mem = &memory[(id * CHUNK_SIZE)..];
-        let end = memory[std::cmp::min((id + 1) * CHUNK_SIZE, file_size)..].as_ptr();
+        let end = memory[std::cmp::min((id + 1) * CHUNK_SIZE, memory.len())..].as_ptr();
 
         // Adjusting start
         if std::intrinsics::likely(id > 0) {
@@ -123,6 +119,7 @@ fn main() -> io::Result<()> {
     let file_path = &args[1];
 
     let file = File::open(file_path)?;
+    file.lock()?;
 
     let count = count_ips(&file);
 
@@ -131,19 +128,27 @@ fn main() -> io::Result<()> {
 }
 
 fn count_ips(file: &File) -> u64 {
-    let file_size = file.metadata().unwrap().len() as usize;
-
     // Create memory-mapped file
+    // Safety: file is locked, so there should not be any UB
     let mmap = Box::leak(Box::new(unsafe { memmap2::Mmap::map(file).unwrap() }));
-    let map: &'static [u8] = unsafe { transmute::<&[u8], &'static [u8]>(mmap.as_ref()) };
 
-    let chunk_id = Box::leak(Box::new(AtomicUsize::new(0)));
-    let bitset = Box::leak(Box::new(MaxBitSet::new()));
+    let chunk_id = AtomicUsize::new(0);
+    let bitset = MaxBitSet::new();
+
+    // leaking these items to make it easy to work with threads
+    // Safety: all this items live till the end of the program
+    let map: &'static [u8] = unsafe { transmute::<&[u8], &'static [u8]>(mmap.as_ref()) };
+    let chunk_id: &'static AtomicUsize = Box::leak(Box::new(chunk_id));
+    let bitset: &'static MaxBitSet = Box::leak(Box::new(bitset));
 
     let mut handles = Vec::new();
     let num_threads = num_cpus::get();
     for _ in 0..num_threads {
-        handles.push(thread::spawn(thread(map, chunk_id, bitset, file_size)));
+        handles.push(thread::spawn(move || {
+            #[cfg(feature = "max_thread_priority")]
+            thread_priority::set_current_thread_priority(thread_priority::ThreadPriority::Max).unwrap();
+            worker(map, chunk_id, bitset);
+        }));
     }
 
     for handle in handles {
@@ -152,48 +157,33 @@ fn count_ips(file: &File) -> u64 {
     bitset.count()
 }
 
-fn thread(
-    map: &'static [u8],
-    chunk_id: &'static AtomicUsize,
-    bitset: &'static MaxBitSet,
-    file_size: usize,
-) -> impl FnOnce() {
-    move || {
-        thread_priority::set_current_thread_priority(thread_priority::ThreadPriority::Max).unwrap();
-        worker(map, chunk_id, bitset, file_size);
-    }
-}
-
 fn num_simd(value: Simd<u8, 16>, new_line: u32) -> u32 {
-    unsafe {
-        let mask = load_mask(new_line);
-        let value = mask.select(value, Simd::splat(0));
-        let hashcode = calculate_hash(new_line, value);
+    let mask = load_mask(new_line);
+    let value = mask.select(value, Simd::splat(0));
+    let hashcode = calculate_hash(new_line, value);
 
-        let pattern_id = PATTERNS_ID[hashcode as usize];
-        let pattern = PATTERNS[pattern_id as usize];
+    let pattern_id = PATTERNS_ID[hashcode as usize];
+    let pattern = &PATTERNS[pattern_id as usize];
 
-        use std::arch::x86_64;
+    let pattern = safe_arch::load_unaligned_m128i(pattern);
 
-        let pattern = x86_64::_mm_loadu_si128(pattern.as_ptr() as *const x86_64::__m128i);
+    let input = safe_arch::m128i::from(value.to_array());
+    let ascii0 = safe_arch::set_splat_i8_m128i('0' as i8);
 
-        let input = x86_64::__m128i::from(value);
-        let ascii0 = x86_64::_mm_set1_epi8('0' as i8);
-
-        let t1 = x86_64::_mm_shuffle_epi8(input, pattern);
-        let t2 = x86_64::_mm_subs_epu8(t1, ascii0);
-        let weights =
-            x86_64::_mm_setr_epi8(10, 1, 10, 1, 10, 1, 10, 1, 100, 0, 100, 0, 100, 0, 100, 0);
-        let t3 = x86_64::_mm_maddubs_epi16(t2, weights);
-        let t4 = x86_64::_mm_alignr_epi8::<8>(t3, t3);
-        let t5 = x86_64::_mm_add_epi16(t4, t3);
-        let t6 = x86_64::_mm_packus_epi16(t5, t5);
-        x86_64::_mm_cvtsi128_si32(t6).cast_unsigned()
-    }
+    let grouped_units_as_chars = safe_arch::shuffle_av_i8z_all_m128i(input, pattern);
+    let grouped_units = safe_arch::sub_saturating_u8_m128i(grouped_units_as_chars, ascii0);
+    let weights = safe_arch::set_reversed_i8_m128i(
+        10, 1, 10, 1, 10, 1, 10, 1, 100, 0, 100, 0, 100, 0, 100, 0,
+    );
+    let units_and_tenths_combined = safe_arch::mul_u8i8_add_horizontal_saturating_m128i(grouped_units, weights);
+    let shifted = safe_arch::combined_byte_shr_imm_m128i::<8>(units_and_tenths_combined, units_and_tenths_combined);
+    let combined = safe_arch::add_i16_m128i(shifted, units_and_tenths_combined);
+    let packed = safe_arch::pack_i16_to_u8_m128i(combined, combined);
+    safe_arch::get_i32_from_m128i_s(packed).cast_unsigned()
 }
 
 fn load_mask(new_line: u32) -> Mask<i8, 16> {
-    simd::Mask::from_bitmask((1u64 << (new_line)).wrapping_sub(1))
+    Mask::from_bitmask((1u64 << (new_line)).wrapping_sub(1))
 }
 
 #[inline]
@@ -205,82 +195,85 @@ fn calculate_hash(new_line: u32, value: Simd<u8, 16>) -> u32 {
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
     use crate::{newline_location, num_simd, read_simd};
     use rand::Rng;
     use std::simd::Simd;
 
-    // #[test]
-    // fn generate_test_file() {
-    //     use rand::Rng;
-    //     use std::fs::File;
-    //     use std::io::{self, Write};
-    //     use std::io::BufWriter;
-    //
-    //     /// Generates `n` random IP addresses and writes them to the specified file.
-    //     fn generate_ips_and_write_to_file(filename: &str, n: usize) -> io::Result<()> {
-    //         let mut file = BufWriter::new(File::create(filename)?);
-    //         let mut rng = rand::thread_rng();
-    //
-    //         for _ in 0..n {
-    //             // Generate a random IP address by creating four random octets.
-    //             let ip = format!(
-    //                 "{}.{}.{}.{}",
-    //                 rng.gen::<u8>(),
-    //                 rng.gen::<u8>(),
-    //                 rng.gen::<u8>(),
-    //                 rng.gen::<u8>()
-    //             );
-    //
-    //             writeln!(file, "{}", ip)?;
-    //         }
-    //
-    //         Ok(())
-    //     }
-    //
-    //     let filename = "target/ips-100.txt";
-    //     let n = 100;
-    //
-    //     generate_ips_and_write_to_file(filename, n).unwrap();
-    //     println!("Generated {} IP addresses and wrote to {}", n, filename);
-    // }
+    /// This is not a test per se
+    /// just a "script" to generate test files to be run on. 
+    #[test]
+    fn generate_test_file() {
+        use rand::Rng;
+        use std::fs::File;
+        use std::io::{self, Write};
+        use std::io::BufWriter;
 
-    /// Converts IP string to an integer bit index.
-    fn ip_to_index_rev(ip: &[u8]) -> Option<u32> {
-        let parts: Vec<&[u8]> = ip.split(|&c| c == '.' as u8).collect();
+        /// Generates `n` random IP addresses and writes them to the specified file.
+        fn generate_ips_and_write_to_file(filename: &str, n: usize) -> io::Result<()> {
+            #[cfg(not(debug_assertions))]
+            if n >= 100000000 {
+                panic!("rerun this test with release mod, otherwise it will take too much time");
+            }
+            let mut file = BufWriter::new(File::create(filename)?);
+            let mut rng = rand::thread_rng();
+
+            for _ in 0..n {
+                // Generate a random IP address by creating four random octets.
+                let ip = format!(
+                    "{}.{}.{}.{}",
+                    rng.gen::<u8>(),
+                    rng.gen::<u8>(),
+                    rng.gen::<u8>(),
+                    rng.gen::<u8>()
+                );
+
+                writeln!(file, "{}", ip)?;
+            }
+
+            Ok(())
+        }
+
+        let sizes = [100, 1000, 1000000, 100000000, 1000000000];
+        for size in sizes {
+            let filename = format!("target/ips-{size}.txt");
+            if !fs::metadata(&filename).is_ok() {
+                generate_ips_and_write_to_file(&filename, size).unwrap();
+            }
+        }
+    }
+
+    fn ip_to_index(ip: &[u8]) -> Option<u32> {
+        let parts: Vec<&[u8]> = ip.split(|&c| c == b'.').collect();
         if parts.len() == 4 {
             let octets: Vec<u8> = parts
                 .iter()
                 .filter_map(|s| String::from_utf8_lossy(s).parse().ok())
                 .collect();
-            if octets.len() == 4 {
-                Some(
-                    (octets[0] as u32)
-                        | ((octets[1] as u32) << 8)
-                        | ((octets[2] as u32) << 16)
-                        | ((octets[3] as u32) << 24),
-                )
-            } else {
-                None
-            }
+            octets_to_u32(octets)
         } else {
             None
         }
     }
 
-    fn ip_to_index_str_rev(ip: &str) -> Option<u32> {
+    fn ip_to_index_str(ip: &str) -> Option<u32> {
         let parts: Vec<&str> = ip.split('.').collect();
         if parts.len() == 4 {
             let octets: Vec<u8> = parts.iter().filter_map(|s| s.parse().ok()).collect();
-            if octets.len() == 4 {
-                Some(
-                    (octets[0] as u32)
-                        | ((octets[1] as u32) << 8)
-                        | ((octets[2] as u32) << 16)
-                        | ((octets[3] as u32) << 24),
-                )
-            } else {
-                None
-            }
+            octets_to_u32(octets)
+        } else {
+            None
+        }
+    }
+
+    fn octets_to_u32(octets: Vec<u8>) -> Option<u32> {
+        if octets.len() == 4 {
+            Some(
+                (octets[0] as u32)
+                    | ((octets[1] as u32) << 8)
+                    | ((octets[2] as u32) << 16)
+                    | ((octets[3] as u32) << 24),
+            )
         } else {
             None
         }
@@ -313,9 +306,9 @@ mod tests {
     #[test]
     fn parse_test_1() {
         let data = Simd::from_array(*b"163.162.45.85\n1.");
-        let location = newline_location((data));
+        let location = newline_location(data);
         let number = num_simd(data, location);
-        let number_real = ip_to_index_rev(&data[..(location as usize)]).unwrap();
+        let number_real = ip_to_index(&data[..(location as usize)]).unwrap();
         assert_eq!(number, number_real);
     }
 
@@ -334,39 +327,6 @@ mod tests {
         let number = num_simd(data, location);
         assert_eq!(number, 0x04030201u32);
     }
-
-    // #[test]
-    // fn hash_test_1() {
-    //     let mut rng = rand::thread_rng();
-    //     let mut ip_str = move || {
-    //         format!(
-    //             "{}.{}.{}.{}",
-    //             rng.gen::<u8>(),
-    //             rng.gen::<u8>(),
-    //             rng.gen::<u8>(),
-    //             rng.gen::<u8>()
-    //         )
-    //     };
-    //     let mut patter_to_hash = HashMap::new();
-    //     for _ in 0..100000 {
-    //         let ip1 = ip_str();
-    //         let ip2 = ip_str();
-    //         let data = format!("{ip1}\n{ip2}\n");
-    //         let data_bytes = data.as_bytes()[0..16].try_into().unwrap();
-    //         let location = newline_location(Simd::from_array(data_bytes));
-    //         let pattern = ip1.replace(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], "x");
-    //         let hash = num2(read_wide_be(&data_bytes), location);
-    //         if !patter_to_hash.contains_key(&pattern) {
-    //             patter_to_hash.insert(pattern, hash);
-    //         } else if patter_to_hash[&pattern] != hash {
-    //             panic!(
-    //                 "Hashes for patter {pattern} do not match {hash} {}\ntested ip memory segment {data}",
-    //                 patter_to_hash[&pattern],
-    //             );
-    //         }
-    //     }
-    //     println!("{patter_to_hash:?}");
-    // }
 
     #[test]
     fn simd_x_x_x_x_test() {
@@ -387,7 +347,7 @@ mod tests {
             let data_bytes = data.as_bytes()[0..16].try_into().unwrap();
             let location = newline_location(Simd::from_array(data_bytes));
             let parsed = num_simd(read_simd(&data_bytes), location);
-            let parsed_correct = ip_to_index_str_rev(&ip1).unwrap();
+            let parsed_correct = ip_to_index_str(&ip1).unwrap();
             assert_eq!(format!("{parsed:#x}"), format!("{parsed_correct:#x}"));
         }
     }
@@ -411,7 +371,7 @@ mod tests {
             let data_bytes = data.as_bytes()[0..16].try_into().unwrap();
             let location = newline_location(Simd::from_array(data_bytes));
             let parsed = num_simd(read_simd(&data_bytes), location);
-            let parsed_correct = ip_to_index_str_rev(&ip1).unwrap();
+            let parsed_correct = ip_to_index_str(&ip1).unwrap();
             assert_eq!(format!("{parsed:#x}"), format!("{parsed_correct:#x}"));
         }
     }
@@ -435,7 +395,7 @@ mod tests {
             let data_bytes = data.as_bytes()[0..16].try_into().unwrap();
             let location = newline_location(Simd::from_array(data_bytes));
             let parsed = num_simd(read_simd(&data_bytes), location);
-            let parsed_correct = ip_to_index_str_rev(&ip1).unwrap();
+            let parsed_correct = ip_to_index_str(&ip1).unwrap();
             assert_eq!(
                 format!("{parsed:#x}"),
                 format!("{parsed_correct:#x}"),
@@ -468,7 +428,7 @@ mod tests {
                 "length mismatch {ip1:?} {location}"
             );
             let number = num_simd(simd_vector, location);
-            let number_real = ip_to_index_str_rev(&ip1).unwrap();
+            let number_real = ip_to_index_str(&ip1).unwrap();
             assert_eq!(number, number_real, "parsing this chunk failed '{data:?}'");
         }
     }
